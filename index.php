6b97e89789b1dfe56c06038bca41ef750f3e7f34
dsdanielsodre <dsdanielsodre@gmail.com>
2022-05-13 17:01:07 -0300

    Conteudo Adicionado

---
+++ index.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- PAGE settings -->
+  <link rel="icon" href="img/logo.png">
+  <title>Raio de Sol Barbearia</title>
+  <meta name="description" content="Raio de Sol Barbearia">
+  <meta name="keywords" content="Raio de Sol Barbearia">
+
+  <!-- ATUALIZAR PAG SEMPRE --> <meta http-equiv="refresh" content="10">
+
+  <!-- CSS dependencies -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="neon.css">
+    <link rel="stylesheet" href="style.css">
+
+  <!-- Script: Make my navbar transparent when the document is scrolled to top -->
+  <script src="js/navbar-ontop.js"></script>
+  <script src="js/animate-in.js"></script>
+
+  <!-- MAPS -->
+  <script src="js/maps.js"></script>
+  <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBQAx6CLr0p1zQFDhB-5lHxn_8X49pkAxE&callback=initMap"></script>
+  <!-- MAPS -->
+</head>
+
+<body>
+  <!-- Navbar -->
+  <!--<nav class="navbar navbar-expand-md navbar-dark ml-0 fixed-top bg-dark" style="">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 ml-1 w-100" style=""><img class="d-block rounded-right rounded-top rounded-bottom rounded-left img-fluid rounded-circle w-25" src="img/logo.png"></div>
+      </div>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar2SupportedContent" aria-controls="navbar2SupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+      <div class="collapse navbar-collapse text-center justify-content-start" id="navbar2SupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item mx-2">
+            <a class="nav-link" href="#features">Features</a>
+          </li>
+          <li class="nav-item mx-2">
+            <a class="nav-link" href="#reviews">Reviews</a>
+          </li>
+        </ul>
+        <a class="btn navbar-btn mx-2 btn-primary shadowed" href="#download">Download</a>
+      </div>
+    </div>
+  </nav>-->
+  <!-- Cover -->
+  <div class="section-fade-out pt-5" style="background-image: url(&quot;img/demou3.jpg&quot;); background-position: center bottom;">
+    <div class="container mt-5 pt-5">
+      <div class="row">
+        <div class="col-md-6 my-5 text-lg-left text-center align-self-center" style="">
+          <h1 class="display-2 text-primary" contenteditable="true" style="">Raio de Sol</h1>
+          <p class="lead">Barbearia</p>
+          <div class="row mt-5">
+            <div class="col-lg-8 col-0" style=""><a class="btn btn-outline-primary w-100" target="_blank" href="https://calendly.com/raiodesolbarbearia">Agendar Atendimento</a></div>
+          </div>
+        </div>
+        <div class="col-lg-6" style="">
+            <a target ="_blank" href="https://www.facebook.com/raiodesol.barber"><img class="img-fluid d-block mx-auto" src="img/mobile  face edit.png" width="400"> </div>
+      </div>
+    </div>
+  </div>
+  <!-- Article style section -->
+  <div class="mt-0 py-5" style="">
+    <div class="container">
+      <div class="row mt-0 py-1" style="">
+        <div class="col-md-6 order-2 order-md-1 animate-in-left mt-0 pt-0" style="">
+          <a target="_blank" href="https://www.instagram.com/raiodesol_barber/"><img class="img-fluid d-block w-75 my-3 mx-auto" src="img/mobile insta.png" width="150" style=""> </div>
+        <div class="col-md-6 align-self-center order-1 order-md-2 text-md-left text-center my-1" style="">
+
+    <h2 style="">Redes Sociais</h2>
+          <p class="text-muted my-2" style="">A Raio de Sol Barbearia é a mais nova barbearia de Embu das Artes, estamos nas principais redes sociais, sigam:</p>
+          <a target="_blank" href="https://www.facebook.com/raiodesol.barber" <="" a="">
+            <i class="fa fa-fw fa-facebook fa-3x text-white"> </i>
+          </a><a target="_blank" href="https://www.instagram.com/raiodesol_barber/" <="" a="">
+            <i class="fa fa-fw fa-instagram fa-3x text-white"></i>
+          </a>
+        </div><a target="_blank" href="https://www.instagram.com/raiodesol_barber/" <="" a="">
+        </a>
+      </div><a target="_blank" href="https://www.instagram.com/raiodesol_barber/" <="" a="">
+
+        <!-- JavaScript dependencies -->
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        <!-- Script: Smooth scrolling between anchors in the same page -->
+        <script src="js/smooth-scroll.js"></script>
+      </a>
+
+      <div class="col-lg-12" style="margin:0px; margin-bottom: 10px;">
+         <div class="localizacao">
+            <hr class="featurette-divider">
+            <h2 class="featurette-heading">LOCALIZAÇÃO<span class="text-muted"></h2>
+            <div id="map"></div>
+            <a href="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap&v=weekly" target="_blank">Estr. São Gonçalo, Estr. São Marcos, 166, Embu das Artes - SP</a>
+             | CEP:  <a href="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap&v=weekly" target="_blank">06814-105</a>
+         </div>
+
+      </div>
+<footer>
+
+       <div class="row" id="caixa">
+          <div class="rodape col-sm-12 col-lg-6">
+             <h2 class="featurette-heading golden">Contato<span class="text-muted"></h2>
+             </p>
+             <h5>
+             Para saber mais sobre a Barbearia Raio de Sol, nossos produtos e serviços entre em contato conosco.
+             <h5>
+             </p>
+             <br>
+             <p>
+             <h5>Fone: <p>
+               (11) 9.1336-4608 (Claro)</h5></p>
+             </p>
+             <p>
+             <h5>Email: </h5>
+             <h5 style="font-size:19px;">contato@barbeariaraiodesol.com </h5>
+
+             </p>
+
+          </div>
+
+          <div class="rodape col-sm-12 col-lg-5">
+             <h2 class="featurette-heading golden">Sobre Nós<span class="text-muted"></h2>
+             <p>
+             <h5> Na Barbearia Raio de Sol, nos dedicamos em fornecer a nossos clientes um serviço cordial.
+                Nós valorizamos a satisfação de nossos clientes.
+             </h5>
+             </p>
+             <p>
+             <h5>Inaugurada em Dezembro/2021, a Barbearia Raio de Sol trás a inovação e o clássico unidos lado a lado.
+             </h5>
+             </p>
+             <p>
+             <h5>Nossa equipe cortês e profissional dispõe de barbeiros com anos de experiência, sem fila de espera e sem stress.
+             </h5>
+             </p>
+          </div>
+
+                 <div class="rodape col-sm-12 col-lg-5">
+
+
+
+                            <img src="img/logodm.png" alt="">
+
+                         </div>
+          <p class="float-right"><a href="#"><img src="img/topo.png" style="width:50px; height: 50px; position:absolute; float: right; margin: auto; right:10px; top:10px"></a></p>
+       </div>
+
+    </footer>
+<div class="copy"> &copy; 2021 Barbearia Raio de Sol. &middot; <a href="#">Privacidade</a> &middot; <a href="#">Termos</a></div>
+    </div>
+
+
+ </div>
+</body>
+
+</html>
